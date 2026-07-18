85c6bc00a82a44b5c735c3321d51ce138cfbeac2
Nirmay rinesh <rineshnirmay1@gmail.com>
2026-07-18 21:01:29 +0530

    Time: 6 ms (64.67%), Space: 45 MB (38.6%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -0,0 +1,36 @@
+class Solution {
+    private final String[] below20 = {
+        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
+        "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen",
+        "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    private final String[] tens = {
+        "", "", "Twenty", "Thirty", "Forty", "Fifty",
+        "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        return helper(num).trim();
+    }
+
+    private String helper(int num) {
+        if (num < 20)
+            return below20[num] + " ";
+
+        if (num < 100)
+            return tens[num / 10] + " " + helper(num % 10);
+
+        if (num < 1000)
+            return helper(num / 100).trim() + " Hundred " + helper(num % 100);
+
+        if (num < 1000000)
+            return helper(num / 1000).trim() + " Thousand " + helper(num % 1000);
+
+        if (num < 1000000000)
+            return helper(num / 1000000).trim() + " Million " + helper(num % 1000000);
+
+        return helper(num / 1000000000).trim() + " Billion " + helper(num % 1000000000);
+    }
+}
