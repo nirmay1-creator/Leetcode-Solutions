772ef72b03f90e1c54c6247bddd77f2eb8a97ad4
Nirmay rinesh <rineshnirmay1@gmail.com>
2026-07-26 20:24:55 +0530

    Time: 324 ms (73.44%), Space: 583.7 MB (38.05%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -61,9 +61,3 @@ public:
     }
 };
 
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary* obj = new WordDictionary();
- * obj->addWord(word);
- * bool param_2 = obj->search(word);
- */
