da7f27759d2c5a27d81c6380baf6d586da26e3a9
Nirmay rinesh <rineshnirmay1@gmail.com>
2026-08-08 20:58:45 +0530

    Time: 70 ms (5.32%), Space: 118.6 MB (15.96%) - LeetHub

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -0,0 +1,96 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+
+        if (m > n) return {};
+
+        vector<int> pre(m, -1), suf(m, -1), sufOne(m, -1);
+
+        // Exact prefix matching
+        int p = 0;
+        for (int j = 0; j < m; j++) {
+            while (p < n && word1[p] != word2[j])
+                p++;
+
+            if (p == n) break;
+
+            pre[j] = p++;
+        }
+
+        // Exact suffix matching
+        p = n - 1;
+        for (int j = m - 1; j >= 0; j--) {
+            while (p >= 0 && word1[p] != word2[j])
+                p--;
+
+            if (p < 0) break;
+
+            suf[j] = p--;
+        }
+
+        // Suffix matching with at most one different character
+        p = n - 1;
+
+        for (int j = m - 1; j >= 0; j--) {
+            while (p >= 0) {
+
+                if (word1[p] == word2[j]) {
+                    sufOne[j] = p--;
+                    break;
+                }
+
+                // Use the one allowed mismatch
+                if (j == 0 || (pre[j - 1] != -1 && pre[j - 1] < p)) {
+                    sufOne[j] = p--;
+                    break;
+                }
+
+                p--;
+            }
+        }
+
+        vector<int> ans;
+
+        p = 0;
+        bool usedMismatch = false;
+
+        for (int j = 0; j < m; j++) {
+
+            while (p < n) {
+
+                // Take exact match
+                if (word1[p] == word2[j]) {
+
+                    if (j == m - 1 || 
+                        (sufOne[j + 1] != -1 && p < sufOne[j + 1])) {
+
+                        ans.push_back(p);
+                        p++;
+                        break;
+                    }
+                }
+
+                else if (!usedMismatch) {
+
+                    if (j == m - 1 || 
+                        (suf[j + 1] != -1 && p < suf[j + 1])) {
+
+                        ans.push_back(p);
+                        p++;
+                        usedMismatch = true;
+                        break;
+                    }
+                }
+
+                p++;
+            }
+
+            if (ans.size() != j + 1)
+                return {};
+        }
+
+        return ans;
+    }
+};
